4933b1b8376fae7cf2573379097c50efc4eacba4
John Naylor <jonaylor89@gmail.com>
2018-09-27 11:38:19 -0400

    Disk sectors

--- boot_sect_main.asm
+++ boot_sect_main.asm
@@ -1,29 +1,31 @@
 
 [org 0x7c00]
+    mov bp, 0x8000 ; set the stack safety away from us
+    mov sp, bp
 
-mov bx, HELLO
-call print
+    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
+    ; the bios sets 'dl' for our boot disk number
+  
+    call disk_load
 
-call print_nl
+    mov dx, [0x9000]
+    call print_hex
 
-mov bx, GOODBYE
-call print
+    call print_hl
 
-call print_nl
+    mov dx, [0x9000 + 512]
+    call print_hex
 
-mov dx, 0x12fe
-call print_hex
-
-jmp $
+    jmp $
 
 %include "boot_sect_print.asm"
 %include "boot_sect_print_hex.asm"
 
-HELLO:
-    db 'Hello, World', 0
-
-GOODBYE:
-    db 'Goodbye', 0
-
-times 510-($-$$) db 0
+;magic number
+times 510 - ($-$$) db 0
 dw 0xaa55
+
+; boot sector = sector 1 of cyl 0 of head of hdd 0
+; from now on = sector 2 ...
+times 256 dw 0xdada ; sector 2 = 512 bytes
+times 256 dw 0xface ; sector 3 = 512 bytes
